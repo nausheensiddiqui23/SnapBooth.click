9e6cb66a3234751ff68be7ccb4d8775cdbe705d6
Nausheen Siddiqui <nausheenzmr@gmail.com>
2025-07-18 23:28:26 +0530

    new msg

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const video = document.getElementById('video');
 const captureBtn = document.getElementById('capture');
+const downloadBtn = document.getElementById('download');
+const strip = document.getElementById('photo-strip');
+const finalCanvas = document.getElementById('final-canvas');
